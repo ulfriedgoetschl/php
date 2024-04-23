f8a23ed8ecbb269ef9ffd2e065c69649f59905f2
ulfriedgoetschl <ulfried.goetschl@gmx.at>
2024-04-23 16:03:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,65 @@
 <?php
+// Database credentials
+$hostname = parse_url(getenv('STACKHERO_MYSQL_HOST'));
+$username = parse_url(getenv('STACKHERO_MYSQL_USER'));
+$password = parse_url(getenv('STACKHERO_MYSQL_PASSWORD'));
+$dbname = parse_url(getenv('STACKHERO_MYSQL_USER'));
 
-echo "Guten Tag";
+// Connect to the database
+$conn = new mysqli($hostname, $username, $password, $dbname);
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
+    exit();
+}
 
+// Fetch data from the "student" table
+$sql = "SELECT * FROM compute";
+$result = $conn->query($sql);
+$row = [];
+
+// Store the fetched data in an associative array
+if ($result->num_rows > 0) {
+    $row = $result->fetch_all(MYSQLI_ASSOC);
+}
+
+// Close the database connection
+mysqli_close($conn);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        td, th {
+            border: 1px solid black;
+            padding: 10px;
+            margin: 5px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>timestamp</th>
+                <th>namespace</th>
+                <th>pod</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if (!empty($row)) {
+                foreach ($row as $rows) {
+                    echo "<tr>";
+                    echo "<td>" . $rows['timestamp'] . "</td>";
+                    echo "<td>" . $rows['namespace'] . "</td>";
+                    echo "<td>" . $rows['pod'] . "</td>";
+                    echo "</tr>";
+                }
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
